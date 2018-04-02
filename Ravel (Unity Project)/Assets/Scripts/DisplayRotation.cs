aa5b1c1f7bac860b92f8a0325fb46af7258a7793
Fireheart182 <cweissgl@andrew.cmu.edu>
2018-04-02 15:52:22 -0400

    Rotational display script

---
+++ Ravel (Unity Project)/Assets/Scripts/DisplayRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    void Update(){
+        transform.Rotate(new Vector3(0, 3 * Time.deltaTime, 0),  Space.World);
+    }
+}
